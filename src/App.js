4d40d523323f1f24cea840c056e5e9d6ac600fa9
plantstoen <plantstoen@gmail.com>
2020-01-09 23:40:19 +0900

    Add: news list, category select bar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const getAPItest = async () => {
-    console.log('Start');
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=f37b334c64974240bda6c5251b0acea0',
-      );
-      setData(response.data);
-      console.log('Done');
-    } catch (e) {
-      console.log(e);
-    }
-  };
   return (
-    <div>
-      <div>
-        <button onClick={getAPItest}>load</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <>
+      <Categories />
+      <NewsList />
+    </>
   );
 };
 
